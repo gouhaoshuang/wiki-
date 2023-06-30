0974937a6ae0c725c9966646fb904444688d157c
ghs <3171392318@qq.com>
2023-06-30 19:20:44 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/example/demo_3/controller/EbookController.java
+++ src/main/java/com/example/demo_3/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.example.demo_3.controller;
 
-import com.example.demo_3.domain.Ebook;
+import com.example.demo_3.req.EbookReq;
 import com.example.demo_3.resp.CommonResp;
+import com.example.demo_3.resp.EbookResp;
 import com.example.demo_3.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/ebook/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list =  ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list =  ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
