46c32ffa88b929803847e5bd5908c9d934a8830a
ghs <3171392318@qq.com>
2023-06-30 19:51:22 +0800

    3-8 制作了CopyUtil 封装BeanUtils

--- src/main/java/com/example/demo_3/service/EbookService.java
+++ src/main/java/com/example/demo_3/service/EbookService.java
@@ -5,11 +5,10 @@ import com.example.demo_3.domain.EbookExample;
 import com.example.demo_3.mapper.EbookMapper;
 import com.example.demo_3.req.EbookReq;
 import com.example.demo_3.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.example.demo_3.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,7 +16,6 @@ public class EbookService {
 
     @Resource
     private EbookMapper ebookMapper;
-
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
@@ -25,15 +23,16 @@ public class EbookService {
 
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
 
-            BeanUtils.copyProperties(ebook,ebookResp);
-
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+//            //对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook,EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
-
 }
