498e6df29c7d070a1488502a2e30764f2e518efd
ghs <3171392318@qq.com>
2023-07-01 17:22:37 +0800

    4-7  制作the-footer，the-header组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <a-layout id="components-layout-demo-top-side-2">
-<!--    上部布局-->
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">fwefw 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
     <router-view/>
-<!--    底部布局-->
-    <a-layout-footer style="text-align: center">
-      jiawa电子书
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from "@/components/the-header.vue";
+import TheFooter from "@/components/the-footer.vue";
+export default defineComponent({
+  name: 'app',
+  components:{
+    TheHeader,
+    TheFooter
+  },
+
+});
+</script>
+
 
 <style>
 #components-layout-demo-top-side-2 .logo {
@@ -31,14 +31,8 @@
   background: rgba(255, 255, 255, 0.3);
 }
 
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
 
-.site-layout-background {
-  background: #fff;
-}
 </style>
-<script setup lang="ts">
-</script>
+
+
+
