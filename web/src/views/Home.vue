8e46d53d6070d62b9b5b78160bdb00ecb9eb663b
ghs <3171392318@qq.com>
2023-07-02 19:15:12 +0800

    5-5 配置axios拦截器打印请求参数和返回参数：

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -92,12 +92,13 @@ export default defineComponent({
   name: 'Home',
 
   setup(){
+    console.log("set up")
     const ebooks = ref();
     const ebooks1 = reactive({books:[]});
     onMounted(()=>{
-      console.log("onMounted1121");
+
       axios.get("/ebook/list").then((response)=>{
-        console.log(response);
+
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
