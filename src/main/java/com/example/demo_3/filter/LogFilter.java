c7b7c1bf77a9b65f1e4c25175f439422dc98a6f2
ghs <3171392318@qq.com>
2023-07-02 22:39:29 +0800

    5-9 增加AOP，打印接口耗时，请求参数和返回参数去掉过滤器，拦截器

--- src/main/java/com/example/demo_3/filter/LogFilter.java
+++ src/main/java/com/example/demo_3/filter/LogFilter.java
@@ -1,48 +1,48 @@
-package com.example.demo_3.filter;// package com.jiawa.wiki.filter;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-
-@Component
-public class LogFilter implements Filter {
-
-    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        // 打印请求信息
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        LOG.info("------------- LogFilter 开始 -------------");
-        LOG.info("fwefwefwefwefwe址: {} {}", request.getRequestURL().toString(), request.getMethod());
-
-        LOG.info("远程地址: {}", request.getRemoteAddr());
-
-        ServletInputStream inputStream = request.getInputStream();
-
-        InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-        BufferedReader bfReader = new BufferedReader(reader);
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = bfReader.readLine()) != null){
-            sb.append(line);
-        }
-        LOG.info("请求参数：{}",sb.toString());
-
-        long startTime = System.currentTimeMillis();
-        filterChain.doFilter(servletRequest, servletResponse);
-        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
-    }
-}
+//package com.example.demo_3.filter;// package com.jiawa.wiki.filter;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.nio.charset.StandardCharsets;
+//
+//@Component
+//public class LogFilter implements Filter {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        // 打印请求信息
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        LOG.info("------------- LogFilter 开始 -------------");
+//        LOG.info("fwefwefwefwefwe址: {} {}", request.getRequestURL().toString(), request.getMethod());
+//
+//        LOG.info("远程地址: {}", request.getRemoteAddr());
+//
+//        ServletInputStream inputStream = request.getInputStream();
+//
+//        InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+//        BufferedReader bfReader = new BufferedReader(reader);
+//        StringBuilder sb = new StringBuilder();
+//        String line;
+//        while ((line = bfReader.readLine()) != null){
+//            sb.append(line);
+//        }
+//        LOG.info("请求参数：{}",sb.toString());
+//
+//        long startTime = System.currentTimeMillis();
+//        filterChain.doFilter(servletRequest, servletResponse);
+//        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+//    }
+//}
