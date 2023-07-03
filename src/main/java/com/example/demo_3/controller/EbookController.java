689f758c5ab2cd73619f8bc49e2c38c6c66cf117
ghs <3171392318@qq.com>
2023-07-03 22:31:13 +0800

    6-9 实现雪花算法，实现增加电子书功能

--- src/main/java/com/example/demo_3/controller/EbookController.java
+++ src/main/java/com/example/demo_3/controller/EbookController.java
@@ -7,8 +7,6 @@ import com.example.demo_3.resp.EbookQueryResp;
 import com.example.demo_3.resp.PageResp;
 import com.example.demo_3.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -26,8 +24,8 @@ public class EbookController {
         resp.setContent(list);
         return resp;
     }
-    @PostMapping("/ebook/save")
-    public CommonResp save(@RequestBody EbookSaveReq req){
+    @GetMapping("/ebook/save")
+    public CommonResp save(EbookSaveReq req){
         CommonResp resp = new CommonResp<>();
         ebookService.save(req);
         return resp;
