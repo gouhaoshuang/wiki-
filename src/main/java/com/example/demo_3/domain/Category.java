4bbecf778969c538eb6549e57afc4f586cad47f7
ghs <3171392318@qq.com>
2023-07-04 19:34:14 +0800

    7-2 分类表设计与持久层代码生成。基本的无限级树型表结构设计：id+parent

---
+++ src/main/java/com/example/demo_3/domain/Category.java
@@ -0,0 +1,48 @@
+package com.example.demo_3.domain;
+
+import java.util.Date;
+
+public class Category {
+    private Byte categoryId;
+
+    private String name;
+
+    private Date lastUpdate;
+
+    public Byte getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Byte categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", categoryId=").append(categoryId);
+        sb.append(", name=").append(name);
+        sb.append(", lastUpdate=").append(lastUpdate);
+        sb.append("]");
+        return sb.toString();
+    }
+}
