a5ef14daa128441e7db5777b740a4695aef25c68
ghs <3171392318@qq.com>
2023-07-05 15:34:04 +0800

    8-2 增加文档表SQL，并生成持久层代码

---
+++ src/main/java/com/example/demo_3/domain/Doc.java
@@ -0,0 +1,90 @@
+package com.example.demo_3.domain;
+
+public class Doc {
+    private Long id;
+
+    private Long ebookId;
+
+    private String parent;
+
+    private String name;
+
+    private String sort;
+
+    private String viewCount;
+
+    private String voteCount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEbookId() {
+        return ebookId;
+    }
+
+    public void setEbookId(Long ebookId) {
+        this.ebookId = ebookId;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(String viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public String getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(String voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", ebookId=").append(ebookId);
+        sb.append(", parent=").append(parent);
+        sb.append(", name=").append(name);
+        sb.append(", sort=").append(sort);
+        sb.append(", viewCount=").append(viewCount);
+        sb.append(", voteCount=").append(voteCount);
+        sb.append("]");
+        return sb.toString();
+    }
+}
