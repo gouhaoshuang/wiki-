248ce42882abbc598c98d8a8a1e5b4a309fe761a
ghs <3171392318@qq.com>
2023-07-06 08:53:48 +0800

    8-8 文档内容表设计与代码生成

--- doc/all.sql
+++ doc/all.sql
@@ -97,4 +97,12 @@ insert into `doc`(id, ebook_id, parent, name,sort,view_count,vote_count) values 
 insert into `doc`(id, ebook_id, parent, name,sort,view_count,vote_count) values (6,1,5, '文档2.2.1',1,0,0);
 
 
-
+-- 文档内容
+drop table if exists `content`;
+create table `content`
+(
+    `id`         bigint not null comment '文档id',
+    `content`   mediumtext not null  comment '内容',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='文档内容';
