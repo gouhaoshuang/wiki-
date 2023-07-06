5edf8e65b3b6e32a23edc5b011c44278e722c5c9
ghs <3171392318@qq.com>
2023-07-06 08:42:19 +0800

    8-7 集成富文本框插件wangEditor@0.15.0，在文档表单中显示富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -67,16 +67,14 @@
             tree-default-expand-all
             :replaceFields="{ label : 'name', key:'id',value:'id'}"
         >
-
         </a-tree-select>
-
       </a-form-item>
-
-
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort"/>
       </a-form-item>
-
+      <a-form-item label="编辑框">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
   </a-modal>
 
@@ -89,13 +87,16 @@ import axios from 'axios';
 import {message} from 'ant-design-vue';
 import {Tool} from "@/util/tool";
 import { useRoute } from 'vue-router';
+import E from "wangeditor"
 
 export default defineComponent({
   name: 'AdminDoc',
 
   setup() {
+
     const route = useRoute();
     console.log("路由",route);
+
 
     const param = ref();
     param.value = {};
@@ -176,6 +177,7 @@ export default defineComponent({
     const doc = ref();
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/doc/save", doc.value).then((response) => {
@@ -209,6 +211,10 @@ export default defineComponent({
 
       //为选择树添加一个“无”
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        const editor = new E("#content");
+        editor.create();
+      },100);
     }
     /**
      *新增
@@ -222,6 +228,10 @@ export default defineComponent({
       treeSelectData.value = Tool.copy(level1.value);
       //为选择树添加一个“无”
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        const editor = new E("#content");
+        editor.create();
+      },1000);
     }
     const ids : Array<string>=[];
     const getDeleteIds = (treeSelectData: any, id: any) => {
@@ -300,6 +310,8 @@ export default defineComponent({
 
 
     onMounted(() => {
+
+
           handleQuery();
         }
     )
