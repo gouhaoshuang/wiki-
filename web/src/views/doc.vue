239a9a1364ac1b3f3118b9ef4a7be2881beb1b05
ghs <3171392318@qq.com>
2023-07-06 20:43:20 +0800

    8-13 打开文档页面时，默认选中第一个节点

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -9,6 +9,7 @@
               @select="onSelect"
               :replaceFields="{title: 'name', key: 'id', value: 'id'}"
               :defaultExpandAll="true"
+              :defaultSelectedKeys="defaultSelectedKeys"
           >
 
           </a-tree>
@@ -35,6 +36,8 @@ export default defineComponent({
     const route = useRoute();
     const docs = ref();
     const html = ref();
+    const defaultSelectedKeys = ref();
+    defaultSelectedKeys.value = [];
     /**
      * 一级文档树，children属性就是二级文档
      * [{
@@ -83,16 +86,14 @@ export default defineComponent({
           docs.value = data.content;
 
           level1.value = [];
-
           level1.value = Tool.array2Tree(docs.value, 0);
 
-
-          // if (Tool.isNotEmpty(level1)) {
-          //   defaultSelectedKeys.value = [level1.value[0].id];
-          //   handleQueryContent(level1.value[0].id);
-          //   // 初始显示文档信息
-          //   doc.value = level1.value[0];
-          // }
+          if (Tool.isNotEmpty(level1)) {
+            defaultSelectedKeys.value = [level1.value[0].id];
+            handleQueryContent(level1.value[0].id);
+            // 初始显示文档信息
+            // doc.value = level1.value[0];
+          }
         } else {
           message.error(data.message);
         }
@@ -120,8 +121,8 @@ export default defineComponent({
     return {
       level1,
       html,
-      onSelect
-      // defaultSelectedKeys,
+      onSelect,
+      defaultSelectedKeys
       // doc,
       // vote
     }
