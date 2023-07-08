c1660595ea18c63b0f4e27865aa2c8ed9ff80f3f
ghs <3171392318@qq.com>
2023-07-08 17:32:10 +0800

    11-6 后端增加获取统计数值接口

--- src/main/java/com/example/demo_3/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/example/demo_3/mapper/EbookSnapshotMapperCust.java
@@ -2,11 +2,15 @@ package com.example.demo_3.mapper;
 
 //import com.example.demo_3.resp.StatisticResp;
 
+import com.example.demo_3.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
 
-//    List<StatisticResp> getStatistic();
+    List<StatisticResp> getStatistic();
 
 //    List<StatisticResp> get30Statistic();
 }
--- src/main/java/com/example/demo_3/service/EbookSnapshotService.java
+++ src/main/java/com/example/demo_3/service/EbookSnapshotService.java
@@ -1,9 +1,11 @@
 package com.example.demo_3.service;
 
 import com.example.demo_3.mapper.EbookSnapshotMapperCust;
+import com.example.demo_3.resp.StatisticResp;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class EbookSnapshotService {
@@ -18,9 +20,9 @@ public class EbookSnapshotService {
     /**
      * 获取首页数值数据：总阅读数、总点赞数、今日阅读数、今日点赞数、今日预计阅读数、今日预计阅读增长
      */
-//    public List<StatisticResp> getStatistic() {
-//        return ebookSnapshotMapperCust.getStatistic();
-//    }
+    public List<StatisticResp> getStatistic() {
+        return ebookSnapshotMapperCust.getStatistic();
+    }
 //
 //    /**
 //     * 30天数值统计
